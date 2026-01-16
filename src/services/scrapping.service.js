4ba5985b52e15d19376bec86892e0a321f0506f8
CythExCodinG <cytherohit01@gmail.com>
2026-01-16 13:45:07 +0530

    path conflict

--- src/services/scrapping.service.js
+++ src/services/scrapping.service.js
@@ -6,7 +6,9 @@ async function scrapper(url) {
   try {
     browser = await puppeteer.launch({
       headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+      args: ["--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage"]
     });
 
     const page = await browser.newPage();
